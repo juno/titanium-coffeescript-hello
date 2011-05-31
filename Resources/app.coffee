5c288db16531ca7f6e9c70cf761809253740688f
Junya Ogura <junyaogura@gmail.com>
2011-06-01 01:30:08 +0900

    add functionality to load twitter search results.

--- Resources/app.coffee
+++ Resources/app.coffee
@@ -1,62 +1,82 @@
-# this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor '#000'
+log = (obj) -> Ti.API.log obj
 
-# create tab group
-tabGroup = Titanium.UI.createTabGroup()
+Ti.UI.setBackgroundColor '#000'
 
-
-#
-# create base UI tab and root window
-#
-win1 = Titanium.UI.createWindow
-  title: 'Tab 1'
+win = Ti.UI.createWindow
+  title: 'Tweets'
   backgroundColor: '#fff'
+  barColor: '#385292'
 
-tab1 = Titanium.UI.createTab
+data = []
+
+createTweetRow = (tweet) ->
+  tweet_text = tweet.text
+  tweet_user = tweet.from_user
+  tweet_avatar = tweet.profile_image_url
+
+  row = Ti.UI.createTableViewRow()
+  row.selectedBackgroundColor = '#fff'
+  row.height = 'auto'
+  row.className = 'datarow'
+  row.clickName = 'row'
+
+  avatar = Ti.UI.createImageView
+    image: tweet_avatar
+    top: 5
+    left: 10
+    width: 48
+    height: 48
+  row.add avatar
+
+  user = Ti.UI.createLabel
+    color: '#576996'
+    font:
+      fontSize: 16
+      fontWeight: 'bold'
+      fontFamily: 'Arial'
+    left: 70
+    top: 0
+    height: 'auto'
+    width: 220
+    text: tweet_user
+  row.add user
+
+  tweet = Ti.UI.createLabel
+    color: '#222'
+    font:
+      fontSize: 14
+      fontWeight: 'normal'
+      fontFamily: 'Arial'
+    left: 70
+    top: 21
+    bottom: 5
+    height: 'auto'
+    width: 220
+    text: tweet_text
+  row.add tweet
+
+  data.push row
+
+loadTweets = ->
+  url = 'http://search.twitter.com/search.json?q=%23titaniumjp'
+  loader = Ti.Network.createHTTPClient()
+  loader.open "GET", url
+  loader.onload = ->
+    response = eval '(' + this.responseText + ')'
+    createTweetRow tweet for tweet in response.results
+    tableView = Ti.UI.createTableView
+      data: data
+      filterAttribute: 'filter'
+      backgroundColor: 'white'
+    win.add tableView
+  loader.send()
+
+loadTweets()
+
+tabGroup = Ti.UI.createTabGroup()
+tab1 = Ti.UI.createTab
   icon:'KS_nav_views.png',
-  title:'Tab 1',
-  window:win1
-
-label1 = Titanium.UI.createLabel
-  color: '#999'
-  text: 'I am Window 1'
-  font:
-    fontSize: 20
-    fontFamily: 'Helvetica Neue'
-  textAlign: 'center'
-  width: 'auto'
-
-win1.add label1
-
-#
-# create controls tab and root window
-#
-win2 = Titanium.UI.createWindow
-  title: 'Tab 2'
-  backgroundColor: '#fff'
-
-tab2 = Titanium.UI.createTab
-  icon: 'KS_nav_ui.png'
-  title: 'Tab 2'
-  window: win2
-
-label2 = Titanium.UI.createLabel
-  color: '#999'
-  text: 'I am Window 2'
-  font:
-    fontSize: 20
-    fontFamily: 'Helvetica Neue'
-  textAlign: 'center'
-  width: 'auto'
-
-win2.add label2
-
-
-#
-# add tabs
-#
+  title:'#titaniumjp',
+  window:win
 tabGroup.addTab tab1
-tabGroup.addTab tab2
-
-# open tab group
 tabGroup.open()
--- Resources/app.js
+++ Resources/app.js
@@ -1,48 +1,93 @@
 (function() {
-  var label1, label2, tab1, tab2, tabGroup, win1, win2;
-  Titanium.UI.setBackgroundColor('#000');
-  tabGroup = Titanium.UI.createTabGroup();
-  win1 = Titanium.UI.createWindow({
-    title: 'Tab 1',
-    backgroundColor: '#fff'
+  var createTweetRow, data, loadTweets, log, tab1, tabGroup, win;
+  log = function(obj) {
+    return Ti.API.log(obj);
+  };
+  Ti.UI.setBackgroundColor('#000');
+  win = Ti.UI.createWindow({
+    title: 'Tweets',
+    backgroundColor: '#fff',
+    barColor: '#385292'
   });
-  tab1 = Titanium.UI.createTab({
+  data = [];
+  createTweetRow = function(tweet) {
+    var avatar, row, tweet_avatar, tweet_text, tweet_user, user;
+    tweet_text = tweet.text;
+    tweet_user = tweet.from_user;
+    tweet_avatar = tweet.profile_image_url;
+    row = Ti.UI.createTableViewRow();
+    row.selectedBackgroundColor = '#fff';
+    row.height = 'auto';
+    row.className = 'datarow';
+    row.clickName = 'row';
+    avatar = Ti.UI.createImageView({
+      image: tweet_avatar,
+      top: 5,
+      left: 10,
+      width: 48,
+      height: 48
+    });
+    row.add(avatar);
+    user = Ti.UI.createLabel({
+      color: '#576996',
+      font: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontFamily: 'Arial'
+      },
+      left: 70,
+      top: 0,
+      height: 'auto',
+      width: 220,
+      text: tweet_user
+    });
+    row.add(user);
+    tweet = Ti.UI.createLabel({
+      color: '#222',
+      font: {
+        fontSize: 14,
+        fontWeight: 'normal',
+        fontFamily: 'Arial'
+      },
+      left: 70,
+      top: 21,
+      bottom: 5,
+      height: 'auto',
+      width: 220,
+      text: tweet_text
+    });
+    row.add(tweet);
+    return data.push(row);
+  };
+  loadTweets = function() {
+    var loader, url;
+    url = 'http://search.twitter.com/search.json?q=%23titaniumjp';
+    loader = Ti.Network.createHTTPClient();
+    loader.open("GET", url);
+    loader.onload = function() {
+      var response, tableView, tweet, _i, _len, _ref;
+      response = eval('(' + this.responseText + ')');
+      _ref = response.results;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        tweet = _ref[_i];
+        createTweetRow(tweet);
+      }
+      tableView = Ti.UI.createTableView({
+        data: data,
+        filterAttribute: 'filter',
+        backgroundColor: 'white'
+      });
+      return win.add(tableView);
+    };
+    return loader.send();
+  };
+  loadTweets();
+  tabGroup = Ti.UI.createTabGroup();
+  tab1 = Ti.UI.createTab({
     icon: 'KS_nav_views.png',
-    title: 'Tab 1',
-    window: win1
+    title: '#titaniumjp',
+    window: win
   });
-  label1 = Titanium.UI.createLabel({
-    color: '#999',
-    text: 'I am Window 1',
-    font: {
-      fontSize: 20,
-      fontFamily: 'Helvetica Neue'
-    },
-    textAlign: 'center',
-    width: 'auto'
-  });
-  win1.add(label1);
-  win2 = Titanium.UI.createWindow({
-    title: 'Tab 2',
-    backgroundColor: '#fff'
-  });
-  tab2 = Titanium.UI.createTab({
-    icon: 'KS_nav_ui.png',
-    title: 'Tab 2',
-    window: win2
-  });
-  label2 = Titanium.UI.createLabel({
-    color: '#999',
-    text: 'I am Window 2',
-    font: {
-      fontSize: 20,
-      fontFamily: 'Helvetica Neue'
-    },
-    textAlign: 'center',
-    width: 'auto'
-  });
-  win2.add(label2);
   tabGroup.addTab(tab1);
-  tabGroup.addTab(tab2);
   tabGroup.open();
 }).call(this);
